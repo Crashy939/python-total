93e6f79f0fbb9e649a553df9ecb895d98354c6a0
Crashy939 <78722591+Crashy939@users.noreply.github.com>
2025-01-04 21:44:38 -0600

    Agregando el día 1

---
+++ DIA1/(1)FuncionPrint/PrimerProyecto.py
@@ -0,0 +1,7 @@
+print("¡Hola, Mundo!")
+#Para imprimir con comillas, se usan las comillas simples o dobles
+print("Hola, mundo, te digo 'hola'")
+#También se pueden imprimir números
+print(1)
+#Y operaciones
+print(100 + 50)
