4f214a59dde6a89c8669890d1677140957439330
Crashy939 <78722591+Crashy939@users.noreply.github.com>
2025-01-08 21:48:06 -0600

    Agregar primer tema variables.py

---
+++ DIA2/(1)Variables/variables.py
@@ -0,0 +1,27 @@
+'''Una variable es un espacio asignado en memoria que almacena un valor, este valor puede cambiar con el
+tiempo y hay varios tipos de variables.'''
+
+#Integer (entero), tienen la particularidad de ser números enteros.
+num1 = 10
+print(num1)
+
+#String (Cadena de texto), es una agrupación de letras que pueden incluir espacios y símbolos.
+nombre = "Javier Mileto Torres Carreon"
+
+#Float (números flotantes o decimales), es un tipo de dato que permite valores decimales.
+num2 = 4.25
+
+#Boolean (buleano), es un tipo de dato que solo puede almacenar dos valores: Verdadero y Falso.
+EsMayor = True
+print("Es mayor:", EsMayor)
+#Las variables pueden cambiar de valor.
+EsMayor = False
+print("Es mayor:", EsMayor)
+
+#Los valores de las variables tienen la propiedad de ser concatenables, esto quiere decir que se pueden
+#sumar dos o más valores, mientras que con los Strings, permite fusionar dos cadenas de texto.
+carita = " :)"
+print(nombre + carita)
+
+print(num1 + num2)
+
