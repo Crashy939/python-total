fd2a6a9faad083dac2433e22f3127dbce1d8babc
Crashy939 <78722591+Crashy939@users.noreply.github.com>
2025-02-24 17:10:51 -0600

    Agregar variables.py

---
+++ DIA2/variables.py
@@ -0,0 +1,16 @@
+nombre = "Juan"
+print(nombre)
+
+nombre = "Laura"
+print(nombre)
+
+edad1 = 10
+edad2 = 15
+print(edad1 + edad2)
+
+nombre1 = input("Dime tu nombre: ")
+print("Tu nombre es: " + nombre1)
+
+texto1 = "Hola "
+frase = texto1 + nombre1
+print(frase)
